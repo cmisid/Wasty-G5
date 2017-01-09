f66a66d87e2a38a20991cd053d5c15b1fcf5dd0e
ls-cd-31 <lucas.clochard@univ-tlse3.fr>
2017-01-09 16:22:26 +0100

    Correction d'une petite erreur de calcul.

--- calc_itin_best_score.py
+++ calc_itin_best_score.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 @author: Groupe[5]
-fichier: score.py version 1.0.0
+fichier: score.py version 1.0.1
+(révision: dans "time_between_nodes", duration est en sec et pas en min)
 """
 
 import itertools
@@ -19,19 +20,20 @@ DURATION_FACTOR = 0.3 #importance de la duree du trajet
 PRICE_FACTOR = 1 #importance du gain
 
 
-'''
+"""
 ENTREE: time = le temps de depart
-        arrival_node = le noeud destination sous la forme suivante:
+        arrival_node = le noeud destination sous la forme suivante
             [coordonnee, debut contrainte horraire, fin contraite horraire, gain]
-        duration = la duree du trajet entre le depart et la destination
+        duration = la duree du trajet entre le depart et la destination, en s
 OBJECTIF: donner le temps du trajet avec le possible temps d'attente
 SORTIE: la duree totale en minute
-'''
-def timeBetweenNodes(time, arrival_node, duration):
+"""
+def time_between_nodes(time, arrival_node, duration):
     # Cas de figure 1: l'utilisateur arrive avant le debut de la tranche horaire
     # On impose a l'utilisateur d'attendre jusqu'au debut de cette tranche
     if arrival_node[1] != None and (time +
-        datetime.timedelta(minutes = duration + RETRIEVAL_TIME) < arrival_node[1]):
+        datetime.timedelta(seconds = duration) + 
+        datetime.timedelta(minutes = RETRIEVAL_TIME) < arrival_node[1]):
         result = arrival_node[1] - time
         return (result.total_seconds()/60 + RETRIEVAL_TIME)
     else:
@@ -39,7 +41,8 @@ def timeBetweenNodes(time, arrival_node, duration):
         # On lui impose une duree de temps suffisamment penalisante
         # pour l'empecher de passer par ce noeud.
         if arrival_node[2] != None and (time +
-            datetime.timedelta(minutes = duration + RETRIEVAL_TIME)) > arrival_node[2]:
+            datetime.timedelta(seconds = duration) +
+            datetime.timedelta(minutes = RETRIEVAL_TIME)) > arrival_node[2]:
             return 100000
         # Cas de figure 3: il arrive a un moment appartenant a la tranche horaire
         # On se retrouve dans ce cas aussi s'il n'y a pas de contrainte horaire
@@ -47,15 +50,15 @@ def timeBetweenNodes(time, arrival_node, duration):
             return (duration/60 + RETRIEVAL_TIME)
 
 
-'''
+"""
 ENTREE: node = tous les noeuds sous forme de liste
         mode_transport = le mode de transport
         gmaps = pour utiliser google api avec la bonne cle
 OBJECTIF: calcule tous les arcs entre les noeuds
 SORTIE: retoure une liste d'arc sous la forme suivante: 
             [1er noeud, 2eme noeud, distance en metre, duree en secondes]
-'''
-def calculatePath(nodes, mode_transport, gmaps):
+"""
+def calculate_path(nodes, mode_transport, gmaps):
   path = []
   for i in nodes:
       for j in nodes:
@@ -68,22 +71,21 @@ def calculatePath(nodes, mode_transport, gmaps):
   return path
 
 
-'''
+"""
 ENTREE: node = tous les noeuds sous forme de liste
         departure = la date et heure de depart
 OBJECTIF: calcule le parcours le plus avantageux
 SORTIE: la liste des noeuds dans le meilleur ordre
-'''
-def shortest_path(nodes, aplha, beta, departure_time):
+"""
+def shortest_path(nodes, departure_time):
     score = 0
     time = 0
     has_best_path = False
     result = []
-    # Recuperatioon de l'API googlemap
+    # Recuperation de l'API googlemap
     gmaps = googlemaps.Client(key='AIzaSyDRpQO4ww7fK610iK5Np-GeiPbCSTuaqec')
     # path_list correspond a l'ensemble des chemins pour chaque paire de noeud.
-    path_list = calculatePath(nodes, "driving", gmaps)
-
+    path_list = calculate_path(nodes, "driving", gmaps)
     # On essaie tous les nombres de noeuds possibles.
     for nb_nodes in range(2,len(nodes) + 1):
         # On passe par toutes les combinaisons possibles
@@ -92,13 +94,13 @@ def shortest_path(nodes, aplha, beta, departure_time):
                 i = 0
                 # On calcule le score pour chaque trajet, a partir du temps et du prix,
                 # en mettant un coefficient discriminant devant chacun des parametres,
-                # et on clcule la somme des scores de tous les trajets.
+                # et on calcule la somme des scores de tous les trajets.
                 # Si on obtient un temps superieur a 100000, le parcours ne sera pas interessant.
                 while (time < 100000) and i < (len(subset) - 1):
                     j = 0
                     while (time < 100000) and j <(len(path_list)):
                         if path_list[j][0] == subset[i] and path_list[j][1] == subset[i + 1]:
-                            time = timeBetweenNodes(departure_time, subset[i + 1] , path_list[j][3])
+                            time = time_between_nodes(departure_time, subset[i + 1] , path_list[j][3])                        
                             departure_time = departure_time + datetime.timedelta(minutes = time)
                             score = score + (DURATION_FACTOR * time - PRICE_FACTOR * subset[i + 1][3])
                         j = j + 1
@@ -115,11 +117,12 @@ def shortest_path(nodes, aplha, beta, departure_time):
                 score = 0
     return result
 
+
 '''
 # Test pour les horaires (heure de depart, contraintes horaires debut et fin)
-dep = datetime.datetime(2017,1,6,11,0,0)
-a = datetime.datetime(2017,1,6,12,0,0)
-b = datetime.datetime(2017,1,6,14,0,0)
+dep = datetime.datetime(2017,1,16,12,0,0)
+a = datetime.datetime(2017,1,16,12,0,0)
+b = datetime.datetime(2017,1,16,14,0,0)
 
 # Ensemble de noeud test
 # Un noeud comprend :
@@ -133,9 +136,9 @@ test_noeud = [[(43.6005543,1.4038282),  None, None,  0],
               [ (43.602729, 1.452065),  None, None,  0],
               [ (43.612238, 1.427174),  None, None,  0]
               ]
-              
+
 test_noeud = [[(43.6005543,1.4038282),  None, None,  0],
-              [ (43.620068, 1.435757),     a,    b, 10],
+              [ (43.620068, 1.435757),     a,    b,  5],
               [ (43.606521, 1.465111),  None, None, 10],
               ]
 
